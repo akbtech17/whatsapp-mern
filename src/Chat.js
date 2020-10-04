9080a64c97785867a85d93517d2182efdc041a7b
anshul bansal <akb.tech17@gmail.com>
2020-10-04 19:17:16 +0530

    created chat header and also basic frame/wall for chat body

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
 import './Chat.css'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 export default function Chat() {
     return (
         <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last Seen...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                
+            </div>
         </div>
     )
 }
