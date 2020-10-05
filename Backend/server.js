cb63f09182ca872795bbf92f98f4ace748f4a510
anshul bansal <akb.tech17@gmail.com>
2020-10-05 07:08:24 +0530

    updated schema(added recieved: bool),added  api route for posting a new whtsapp message and also added middleware

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 //importing
 import express from 'express';
 import mongoose from 'mongoose';
+import Messages from './dbMessages.js'
 
 
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 9000;
 
 
 //middleware
+app.use(express.json());
 
 //DB config
 const connect_url = 'mongodb+srv://anshulbansal17:94@nsH11@cluster0.ullmx.mongodb.net/whatsappdb?retryWrites=true&w=majority';
@@ -23,6 +25,20 @@ mongoose.connect(connect_url, {
 
 //api routes
 app.get('/', (req,res) => res.status(200).send("Hello_world"));
+
+//api route for poting a new whtsapp message 
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.status(201).send(data);
+        }
+    })
+})
 
 //listener
 app.listen(port, ()=> console.log(`Listnening to localhost ${port}`));
